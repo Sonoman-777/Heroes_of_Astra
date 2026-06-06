f48fea6371624228a19a32549c69d9d68b25c52e
Sonoman <son777o777man@gmail.com>
2026-06-06 08:55:07 +0300

    Create ru.js

---
+++ data/texts/ru.js
@@ -0,0 +1,33 @@
+export default {
+    // Главное меню
+    MENU_SINGLEPLAYER: 'Одиночная игра',
+    MENU_CAMPAIGN: 'Кампания',
+    MENU_SCENARIOS: 'Сценарии',
+    MENU_LOAD_GAME: 'Загрузить игру',
+    MENU_MULTIPLAYER: 'Сетевая игра',
+    MENU_HOT_SEAT: 'Одно устройство',
+    MENU_LOCAL_NETWORK: 'Локальная сеть',
+    MENU_DUEL: 'Дуэль',
+    MENU_SETTINGS: 'Настройки',
+    MENU_CREDITS: 'Авторы',
+    MENU_BACK: 'Назад',
+    
+    // Настройки
+    SETTINGS_TITLE: 'НАСТРОЙКИ',
+    SETTINGS_MUSIC: 'Музыка',
+    SETTINGS_SOUND: 'Звуки',
+    SETTINGS_ANIMATION: 'Скорость анимаций',
+    SETTINGS_LANGUAGE: 'Язык',
+    SETTINGS_LANG_RU: 'Русский',
+    SETTINGS_LANG_EN: 'English',
+    
+    // Заставка
+    SPLASH_START: 'НАЧАТЬ ИГРУ',
+    
+    // Авторы
+    CREDITS_TITLE: 'АВТОРЫ',
+    CREDITS_TEXT: 'Пошаговая стратегия, вдохновлённая классической серией Heroes',
+    CREDITS_DEV: 'Разработка и дизайн',
+    CREDITS_VERSION: 'Версия 0.0.1 (альфа)',
+    CREDITS_CLOSE: 'ЗАКРЫТЬ'
+};
