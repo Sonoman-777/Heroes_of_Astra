63a14a94617719a656fbc9c93d06c5ce067946f5
Sonoman <son777o777man@gmail.com>
2026-06-06 08:39:38 +0300

    Create eventBus.js

---
+++ js/eventBus.js
@@ -0,0 +1,36 @@
+class EventBus {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, callback) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(callback);
+        
+        return () => this.off(event, callback);
+    }
+
+    off(event, callback) {
+        if (!this.events[event]) return;
+        this.events[event] = this.events[event].filter(cb => cb !== callback);
+    }
+
+    emit(event, data) {
+        if (!this.events[event]) return;
+        this.events[event].forEach(callback => {
+            try {
+                callback(data);
+            } catch (error) {
+                console.error(`Îøèáêà â îáðàáîò÷èêå ñîáûòèÿ ${event}:`, error);
+            }
+        });
+    }
+
+    clear() {
+        this.events = {};
+    }
+}
+
+export default new EventBus();
